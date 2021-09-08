271a32dee59cb1ed334e0d77c6cbb36c8b28ea99
CorreiaJV <correia.jv@gmail.com>
2021-09-08 18:51:12 -0300

    Criação do método editar

--- src/modelo/Endereco.java
+++ src/modelo/Endereco.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import helper.Util;
+
 public class Endereco {
 	private String estado;
 	private String cidade;
@@ -62,7 +64,89 @@ public class Endereco {
 				+ numero  ;
 	}
 	
-	
+	public void editar(Endereco endereco) {
+
+		System.out.println("1- Editar Estado ");
+		System.out.println("2- Editar Cidade ");
+		System.out.println("3- Editar Bairro ");
+		System.out.println("4- Editar CEP ");
+		System.out.println("5- Editar Número ");
+		System.out.println("6- Editar tudo ");
+
+		int aux = Main.ler.nextInt();
+		Main.ler.nextLine();
+		switch (aux) {
+		case 1:
+			System.out.println("Informe o estado ");
+			String estado = Main.ler.nextLine();
+			endereco.setEstado(estado);
+			System.out.println("Campo alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+			break;
+		case 2:
+			System.out.println("Informe a Cidade");
+			String cidade = Main.ler.nextLine();
+			endereco.setCidade(cidade);;
+			System.out.println("Campo alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+			break;
+		case 3:
+			System.out.println("Informe o bairro ");
+			String bairro = Main.ler.nextLine();
+			endereco.setBairro(bairro);;
+			System.out.println("Campo alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+		case 4:
+			System.out.println("Informe o CEP ");
+			String cep = Main.ler.nextLine();
+			endereco.setCep(cep);
+			System.out.println("Campo alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+		case 5:
+			System.out.println("Informe o Número ");
+			String num = Main.ler.nextLine();
+			endereco.setNumero(num);
+			System.out.println("Campo alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+			break;
+		case 6:
+			System.out.println("Informe o estado ");
+			estado = Main.ler.nextLine();
+
+			System.out.println("Informe a cidade ");
+			cidade = Main.ler.nextLine();
+
+			System.out.println("Informe o bairro");
+			bairro = Main.ler.nextLine();
+			System.out.println("Informe o cep");
+			cep = Main.ler.nextLine();
+			System.out.println("Informe o Número");
+			num = Main.ler.nextLine();
+			
+			endereco.setEstado(estado);
+			endereco.setCidade(cidade);
+			endereco.setBairro(bairro);
+			endereco.setCep(cep);
+			endereco.setNumero(num);
+			
+			System.out.println("Endereço alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+
+			break;
+		default:
+			System.out.println("Opção inválida");
+			Util.pausar(2);
+			this.editar(endereco);
+			break;
+		}
+
+	}
 	
 	
 
--- src/modelo/Telefone.java
+++ src/modelo/Telefone.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import helper.Util;
+
 public class Telefone {
 	private int ddd;
 	private String numero;
@@ -27,5 +29,55 @@ public class Telefone {
 		
 	}
 	
+	public void editar(Telefone telefone) {
+
+		System.out.println("1- Editar DDD ");
+		System.out.println("2- Editar Número ");
+		System.out.println("3- Editar Tudo ");
+		
+
+		int aux = Main.ler.nextInt();
+		Main.ler.nextLine();
+		switch (aux) {
+		case 1:
+			System.out.println("Informe o DDD ");
+			int ddd = Main.ler.nextInt();
+			Main.ler.nextLine();
+			telefone.setDdd(ddd);
+			System.out.println("Campo alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+			break;
+		case 2:
+			System.out.println("Informe o Número");
+			String num = Main.ler.nextLine();
+			telefone.setNumero(num);
+			System.out.println("Campo alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+			break;
+		case 3:
+			System.out.println("Informe o DDD ");
+			ddd = Main.ler.nextInt();
+			Main.ler.nextLine();
+			
+			System.out.println("Informe o Número ");
+			num = Main.ler.nextLine();
+			
+			telefone.setDdd(ddd);
+			telefone.setNumero(num);
+			
+			System.out.println("Telefone alterado com sucesso");
+			Util.pausar(2);
+			//Main.menu();
+		
+		default:
+			System.out.println("Opção inválida");
+			Util.pausar(2);
+			this.editar(telefone);
+			break;
+		}
+
+	}
 	
 }
