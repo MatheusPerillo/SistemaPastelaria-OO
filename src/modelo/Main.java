0c4742d6d190732400e3f3d62abde3427c222fef
CorreiaJV <correia.jv@gmail.com>
2021-09-08 19:00:28 -0300

    Realização de teste de métodos na main

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -1,8 +1,14 @@
 package modelo;
 
+import java.util.Scanner;
+
+import modelo.Funcionario.Cargo;
+
 public class Main {
+	public static Scanner ler = new Scanner(System.in);
+	
 public static void main(String[] args) {
-	System.out.println("Hello World");
+	
 	
 	Endereco end = new Endereco("Distrito federal","Brasília","Band","71710011","04");
 	Telefone tel = new Telefone(61,"99946-8480");
@@ -10,7 +16,10 @@ public static void main(String[] args) {
 	
 	Funcionario perillo = new Funcionario("Perillo",end,tel,"email@email.com",Cargo.Gerente,3565.88,"24/02/2001",10);
 	
-	
-	System.out.println(perillo);
+	System.out.println(tel);
+	tel.editar(tel);
+	System.out.println(tel);
 }
+
+
 }
