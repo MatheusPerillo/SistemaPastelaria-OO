d89a311214b9372c5a0b3333de323f565faed196
Joao Victor C <correia.jv@gmail.com>
2021-09-08 17:25:32 -0300

    Resolvendo conflitos

--- src/modelo/Loja.java
+++ src/modelo/Loja.java
@@ -5,9 +5,18 @@ public class Loja {
 	private String nome;
 	private Endereco endereco;
 	private Telefone telefone;
-	private Funcionario funcionarios;
 	private ArrayList<Cliente> clientes;
+	private  ArrayList<Funcionario>  funcionarios;	
 	private ArrayList<Venda> vendas;
+	
+	
+		
+	
+	public Loja(String nome, Endereco endereco, Telefone telefone) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
 	
 	public String getNome() {
 		return nome;
@@ -27,38 +36,17 @@ public class Loja {
 	public void setTelefone(Telefone telefone) {
 		this.telefone = telefone;
 	}
-	public Funcionario getFuncionarios() {
-		return funcionarios;
-	}
-	public void setFuncionarios(Funcionario funcionarios) {
-		this.funcionarios = funcionarios;
-	}
-	public ArrayList<Cliente> getClientes() {
-		return clientes;
-	}
-	public void setClientes(ArrayList<Cliente> clientes) {
-		this.clientes = clientes;
-	}
-	public ArrayList<Venda> getVendas() {
-		return vendas;
-	}
-	public void setVendas(ArrayList<Venda> vendas) {
-		this.vendas = vendas;
-	}
 	
-	public Loja(String nome, Endereco endereco, Telefone telefone, Funcionario funcionarios,
-			ArrayList<Cliente> clientes, ArrayList<Venda> vendas) {
-		this.nome = nome;
-		this.endereco = endereco;
-		this.telefone = telefone;
-		this.funcionarios = funcionarios;
-		this.clientes = clientes;
-		this.vendas = vendas;
+	public void CadastrarCliente(Cliente cliente) {
+		this.clientes.add(cliente);
 	}
-	public Loja() {
-		super();
-		// TODO Auto-generated constructor stub
+	public void CadastrarFuncionario(Funcionario funcionario) {
+		this.funcionarios.add(funcionario);
 	}
+	public void CadastrarVenda(Venda venda) {
+		this.vendas.add(venda);
+	}
+
 
 	
 }
