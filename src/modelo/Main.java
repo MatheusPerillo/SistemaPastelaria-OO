e5228a7bc3879f7773338b545fb2f6e42641e8f1
Joao Victor C <correia.jv@gmail.com>
2021-09-08 17:25:22 -0300

    Criação da Main para testes dos métodos

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -3,5 +3,14 @@ package modelo;
 public class Main {
 public static void main(String[] args) {
 	System.out.println("Hello World");
+	
+	Endereco end = new Endereco("Distrito federal","Brasília","Band","71710011","04");
+	Telefone tel = new Telefone(61,"99946-8480");
+	Cliente joao = new Cliente("Joao",end,tel,"ui@ui.com","123");
+	
+	Funcionario perillo = new Funcionario("Perillo",end,tel,"email@email.com",Cargo.Gerente,3565.88,"24/02/2001",10);
+	
+	
+	System.out.println(perillo);
 }
 }
