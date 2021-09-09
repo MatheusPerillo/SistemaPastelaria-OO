295009b721c507d65ab7d861814113633ff5a653
MatheusPerillo <c4tr4u1@gmail.com>
2021-09-08 23:59:56 -0300

    Criação de testes para os métodos implementados

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -8,17 +8,18 @@ public class Main {
 	public static Scanner ler = new Scanner(System.in);
 	
 public static void main(String[] args) {
-	System.out.println("Hello World");
+	
 	
 	Endereco end = new Endereco("Distrito federal","Brasília","Band","71710011","04");
-	Telefone tel = new Telefone(61,"99946-8480");
+	Telefone tel = new Telefone("61","99946-8480");
 	Cliente joao = new Cliente("Joao",end,tel,"ui@ui.com","123");
-	
+	Bebida coca = new Bebida("coca", 7.99, "geladinha", 100, 2000, "garrafa");
 	Funcionario perillo = new Funcionario("Perillo",end,tel,"email@email.com",Cargo.Gerente,3565.88,"24/02/2001",10);
 	
-	System.out.println(end);
-	end.editar(end);
-	System.out.println(end);
+	System.out.println(joao);
+	joao.editar(joao);
+	System.out.println(joao);
+	
 }
 
 
