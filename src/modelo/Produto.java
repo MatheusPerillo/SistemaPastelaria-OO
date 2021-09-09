3fe7a5489d5b790f2297df1d3dda9d7f0b18b3e9
MatheusPerillo <c4tr4u1@gmail.com>
2021-09-09 02:20:50 -0300

    Criação e validação do método de repor estoque

--- src/modelo/Produto.java
+++ src/modelo/Produto.java
@@ -5,13 +5,12 @@ import helper.Util;
 public abstract class Produto {
 
 	private int id;
-	private static int contador = 1; 
+	private static int contador = 1;
 	private String nome;
 	private Double valor;
 	private String descricao;
 	private int qtdEstoque;
-	
-	
+
 	public Produto(String nome, Double valor, String descricao, int qtdEstoque) {
 		this.id = Produto.contador;
 		this.nome = nome;
@@ -20,45 +19,54 @@ public abstract class Produto {
 		this.qtdEstoque = qtdEstoque;
 		Produto.contador++;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Double getValor() {
 		return valor;
 	}
+
 	public void setValor(Double valor) {
 		this.valor = valor;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public int getQtdEstoque() {
 		return qtdEstoque;
 	}
+
 	public void setQtdEstoque(int qtdEstoque) {
 		this.qtdEstoque = qtdEstoque;
 	}
-	
+
 	public void verifDisponiblidade() {
 
 		if (getQtdEstoque() > 0) {
 			System.out.println("Quantidade em estoque disponível: " + this.getQtdEstoque());
 		} else {
 			System.out.println("\nNecessita-se de reposição");
-			Util.pausar(2);
+			Util.pausar(1);
 			System.out.println("\nDeseja repor a quantidade em estoque deste produto?\n1- Sim\n2- Não");
 			int aux1 = Main.ler.nextInt();
 			Main.ler.nextLine();
@@ -78,11 +86,19 @@ public abstract class Produto {
 		int rep = Main.ler.nextInt();
 		Main.ler.nextLine();
 
+		do {
+			System.out.println("\nQuantidade inválida!");
+			Util.pausar(1);
+			System.out.println("\nDigite a quantidade para repor o produto: ");
+			rep = Main.ler.nextInt();
+			Main.ler.nextLine();
+
+		} while (rep <= 0);
+
 		this.setQtdEstoque(rep);
 		System.out.println("\nReposição feita com sucesso!");
 		Util.pausar(2);
 
 	}
-	
-	
+
 }
