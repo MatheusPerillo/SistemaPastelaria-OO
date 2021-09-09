324a1131d72c1842fcb1e0d06b2796b70453fad8
MatheusPerillo <c4tr4u1@gmail.com>
2021-09-09 01:37:31 -0300

    Criação e teste dos métodos de verifDisp e solicRepo

--- src/modelo/Pastel.java
+++ src/modelo/Pastel.java
@@ -2,37 +2,40 @@ package modelo;
 
 import helper.Util;
 
-public class Pastel extends Produto{
+public class Pastel extends Produto {
 
 	private String tamanho;
 	private String sabor;
-	
-	public Pastel( String nome, Double valor, String descricao, int qtdEstoque, String tamanho, String sabor) {
-		super( nome, valor, descricao, qtdEstoque);
+
+	public Pastel(String nome, Double valor, String descricao, int qtdEstoque, String tamanho, String sabor) {
+		super(nome, valor, descricao, qtdEstoque);
 		this.tamanho = tamanho;
 		this.sabor = sabor;
 	}
-	
+
 	public String getTamanho() {
 		return tamanho;
 	}
+
 	public void setTamanho(String tamanho) {
 		this.tamanho = tamanho;
 	}
+
 	public String getSabor() {
 		return sabor;
 	}
+
 	public void setSabor(String sabor) {
 		this.sabor = sabor;
 	}
-	
+
 	@Override
 	public String toString() {
-		return  "\nNome: " + this.getNome() + "\nSabor: " + this.getSabor() + "\nTamanho: " + this.getTamanho() +
-				"\nDescrição: " + this.getDescricao() + "\nValor: " + Util.doubleParaString(this.getValor()) + "\nEm estoque: " 
-				+ Integer.toString(getQtdEstoque());
+		return "\nNome: " + this.getNome() + "\nSabor: " + this.getSabor() + "\nTamanho: " + this.getTamanho()
+				+ "\nDescrição: " + this.getDescricao() + "\nValor: " + Util.doubleParaString(this.getValor())
+				+ "\nEm estoque: " + Integer.toString(getQtdEstoque());
 	}
-	
+
 	public void editar(Pastel pastel) {
 		System.out.println("1- Editar Nome");
 		System.out.println("2- Editar Sabor");
@@ -41,10 +44,10 @@ public class Pastel extends Produto{
 		System.out.println("5- Editar Valor");
 		System.out.println("6- Editar Estoque");
 		System.out.println("7- Editar Tudo");
-		
+
 		int aux = Main.ler.nextInt();
 		Main.ler.nextLine();
-		switch(aux) {
+		switch (aux) {
 		case 1:
 			System.out.println("Informe o Nome: ");
 			String nome = Main.ler.nextLine();
@@ -52,7 +55,7 @@ public class Pastel extends Produto{
 			System.out.println("Campo alterado com sucesso!");
 			Util.pausar(2);
 			break;
-			
+
 		case 2:
 			System.out.println("Informe o Sabor: ");
 			String sabor = Main.ler.nextLine();
@@ -60,7 +63,7 @@ public class Pastel extends Produto{
 			System.out.println("Campo alterado com sucesso!");
 			Util.pausar(2);
 			break;
-			
+
 		case 3:
 			System.out.println("Informe o Tamanho: ");
 			String tamanho = Main.ler.nextLine();
@@ -68,7 +71,7 @@ public class Pastel extends Produto{
 			System.out.println("Campo alterado com sucesso!");
 			Util.pausar(2);
 			break;
-			
+
 		case 4:
 			System.out.println("Informe a Descrição: ");
 			String descricao = Main.ler.nextLine();
@@ -76,16 +79,16 @@ public class Pastel extends Produto{
 			System.out.println("Campo alterado com sucesso!");
 			Util.pausar(2);
 			break;
-			
+
 		case 5:
 			System.out.println("Informe o Valor: ");
 			Double valor = Main.ler.nextDouble();
 			Main.ler.nextLine();
 			pastel.setValor(valor);
 			System.out.println("Campo alterado com sucesso!");
-			Util.pausar(2);	
+			Util.pausar(2);
 			break;
-			
+
 		case 6:
 			System.out.println("Informe o Estoque: ");
 			int qtdEstoque = Main.ler.nextInt();
@@ -94,47 +97,46 @@ public class Pastel extends Produto{
 			System.out.println("Campo alterado com sucesso!");
 			Util.pausar(2);
 			break;
-			
+
 		case 7:
 			System.out.println("Informe o Nome: ");
 			nome = Main.ler.nextLine();
-			
+
 			System.out.println("Informe o Sabor: ");
 			sabor = Main.ler.nextLine();
-			
+
 			System.out.println("Informe o Tamanho: ");
 			tamanho = Main.ler.nextLine();
-			
+
 			System.out.println("Informe a Descrição: ");
 			descricao = Main.ler.nextLine();
-			
+
 			System.out.println("Informe o Valor: ");
 			valor = Main.ler.nextDouble();
 			Main.ler.nextLine();
-			
+
 			System.out.println("Informe o Estoque: ");
 			qtdEstoque = Main.ler.nextInt();
 			Main.ler.nextLine();
-			
+
 			pastel.setNome(nome);
 			pastel.setSabor(sabor);
 			pastel.setTamanho(tamanho);
 			pastel.setDescricao(descricao);
 			pastel.setValor(valor);
 			pastel.setQtdEstoque(qtdEstoque);
-			
+
 			System.out.println("Pastel alterado com sucesso!");
 			Util.pausar(2);
 			break;
-			
+
 		default:
 			System.out.println("Opção inválida");
 			Util.pausar(2);
 			this.editar(pastel);
 			break;
 		}
-		
-		
+
 	}
-	
+
 }
