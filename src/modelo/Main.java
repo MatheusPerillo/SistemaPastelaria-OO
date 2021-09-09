ed301d0e3ca2853a5a1e75f04e3b783b7e5c7467
Joao Victor C <correia.jv16@gmail.com>
2021-09-09 13:33:40 -0300

    Implementação dos métodos do Objeto Pedido e refatoração dos atributos

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -1,5 +1,6 @@
 package modelo;
 
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 import modelo.Funcionario.Cargo;
@@ -8,24 +9,34 @@ public class Main {
 	public static Scanner ler = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
+		GregorianCalendar dataDeInscricao = new GregorianCalendar();
 		Endereco end = new Endereco("Distrito federal", "Brasília", "Band", "71710011", "04");
 		Telefone tel = new Telefone("61", "99946-8480");
+		Loja perillao= new Loja("Os brasbos",end,tel);
 		Cliente joao = new Cliente("Joao", end, tel, "ui@ui.com", "123");
-		Pastel flango = new Pastel("frangod", 10.00, "frango de Deus", 0, "G", "frango");
-		Bebida coca = new Bebida("coca", 7.99, "geladinha", 0, 2000, "garrafa");
+		Pastel flango = new Pastel("frangod", 10.00, "frango de Deus", 4, "G", "frango");
+		Pastel flango2 = new Pastel("frangod2", 20.00, "frango de Deus", 2, "G", "frango");
+		Bebida coca = new Bebida("coca", 7.99, "geladinha", 2, 2000, "garrafa");
 		Funcionario perillo = new Funcionario("Perillo", end, tel, "email@email.com", Cargo.Gerente, 3565.88,
 				"24/02/2001", 10);
-
-		System.out.println(coca);
-		coca.verifDisponiblidade();
-		System.out.println(coca);
-
-		System.out.println(perillo);
-
-		perillo.editar(perillo);
-		;
-		System.out.println(perillo);
+		Pedido pedido1 = new Pedido(200.0,joao,perillo,dataDeInscricao.getTime());
+		
+		
+	System.out.println(pedido1.getValorTotal());
+	pedido1.pedirBebida(coca, 2);
+	pedido1.pedirPastel(flango, 2);
+	pedido1.pedirPastel(flango2, 3);
+	pedido1.calcularValorTotal();
+	
+	pedido1.listarBebidasPedidas();
+	pedido1.listarPasteisPedidos();
+	
+	System.out.println("\n\n"+pedido1.getValorTotal());
+	
+		
+		
+		
+		
 
 	}
 
--- src/modelo/Pedido.java
+++ src/modelo/Pedido.java
@@ -1,27 +1,29 @@
 package modelo;
-import java.util.ArrayList;
+
+
 import java.util.Date;
+import java.util.HashMap;
+
+import java.util.Map;
+
 
 public class Pedido {
 	private int id;
-	private ArrayList<Pastel> pastel;
-	private ArrayList<Bebida> bebida;
-	private int qtdPedida;
-	private Float valorTotal;
+	private static int contador;
+	private HashMap<Pastel,Integer> pasteis = new HashMap<>();
+	private HashMap<Bebida,Integer> bebidas = new HashMap<>();
+	private Double valorTotal;
 	private Cliente cliente;
 	private Funcionario vendedor;
 	private Date dataPedido;
 	
-	public Pedido(int id, ArrayList<Pastel> pastel, ArrayList<Bebida> bebida, int qtdPedida, Float valorTotal,
-			Cliente cliente, Funcionario vendedor, Date dataPedido) {
-		this.id = id;
-		this.pastel = pastel;
-		this.bebida = bebida;
-		this.qtdPedida = qtdPedida;
+	public Pedido( Double valorTotal,Cliente cliente, Funcionario vendedor, Date dataPedido) {
+		this.id = Pedido.contador;		
 		this.valorTotal = valorTotal;
 		this.cliente = cliente;
 		this.vendedor = vendedor;
 		this.dataPedido = dataPedido;
+		contador++;
 	}
 	
 	public int getId() {
@@ -30,28 +32,28 @@ public class Pedido {
 	public void setId(int id) {
 		this.id = id;
 	}
-	public ArrayList<Pastel> getPastel() {
-		return pastel;
+
+	
+	public HashMap<Pastel, Integer> getPasteis() {
+		return pasteis;
 	}
-	public void setPastel(ArrayList<Pastel> pastel) {
-		this.pastel = pastel;
+
+	public void setPasteis(HashMap<Pastel, Integer> pasteis) {
+		this.pasteis = pasteis;
 	}
-	public ArrayList<Bebida> getBebida() {
-		return bebida;
+
+	public HashMap<Bebida, Integer> getBebidas() {
+		return bebidas;
 	}
-	public void setBebida(ArrayList<Bebida> bebida) {
-		this.bebida = bebida;
+
+	public void setBebidas(HashMap<Bebida, Integer> bebidas) {
+		this.bebidas = bebidas;
 	}
-	public int getQtdPedida() {
-		return qtdPedida;
-	}
-	public void setQtdPedida(int qtdPedida) {
-		this.qtdPedida = qtdPedida;
-	}
-	public Float getValorTotal() {
+
+	public Double getValorTotal() {
 		return valorTotal;
 	}
-	public void setValorTotal(Float valorTotal) {
+	public void setValorTotal(Double valorTotal) {
 		this.valorTotal = valorTotal;
 	}
 	public Cliente getCliente() {
@@ -73,7 +75,71 @@ public class Pedido {
 		this.dataPedido = dataPedido;
 	}
 	
+	public void pedirPastel(Pastel pastel, int qtd) {
+		if(pastel.getQtdEstoque() >= qtd) {
+		this.pasteis.put(pastel, qtd);		
+		System.out.println("Pastel: " + pastel.getNome()+ " Pedido com sucesso" + "\nQtd pedida: " + qtd  );
+		}else {System.out.println("\nNão há produto em estoque suficiente para o pedido");}	
+		
+	}	
+	public void pedirBebida(Bebida bebida, int qtd) {
+		this.bebidas.put(bebida, qtd);
+		System.out.println("Bebida: " + bebida.getNome()+ " Pedida com sucesso" + "\nQtd pedida: " + qtd  );
+	}
 	
+	public void listarPasteisPedidos() {
+		System.out.println("\nPasteis pedidos: ");
+		for(Map.Entry<Pastel, Integer> entrada : pasteis.entrySet()) {
+				System.out.println(entrada.getKey() + "\nQtd Pedida: " +entrada.getValue() );		
+			}	
+	}
 	
+	public void listarBebidasPedidas() {
+		System.out.println("\nBebidas pedidas: ");
+		for(Map.Entry<Bebida, Integer> entrada :bebidas.entrySet()) {
+			System.out.println(entrada.getKey() + "\nQtd Pedida: " +entrada.getValue() );		
+		}
+	}
 	
+	public void listarProdutosPedidos() {
+		this.listarBebidasPedidas();
+		this.listarPasteisPedidos();
+	}
+	
+	public void AlterarPastelPedido(Pastel pastel, Pastel novoPastel) {
+		for(Map.Entry<Pastel, Integer> entrada : pasteis.entrySet()) {
+				if(pastel.equals(entrada.getKey())) {
+					pasteis.remove(entrada.getKey());
+					System.out.println("\nDigite a nova quantidade:");
+					int qtd = Main.ler.nextInt();
+					Main.ler.nextLine();
+					this.pedirPastel(novoPastel, qtd);
+				}		
+			}	
+		
+	}
+	public void AlterarBebidaPedida(Bebida bebida, Bebida novaBebida) {
+		for(Map.Entry<Bebida, Integer> entrada : bebidas.entrySet()) {
+				if(bebida.equals(entrada.getKey())) {
+					bebidas.remove(entrada.getKey());
+					System.out.println("\nDigite a nova quantidade:");
+					int qtd = Main.ler.nextInt();
+					Main.ler.nextLine();
+					this.pedirBebida(novaBebida, qtd);
+				}		
+			}	
+	}
+	public void calcularValorTotal() {
+		double valorTotal=0;
+		double valorBebidas=0;
+		double valorPasteis=0;
+		for(Map.Entry<Bebida, Integer> entrada : bebidas.entrySet()) {
+			valorPasteis += entrada.getValue() * entrada.getKey().getValor();
+		}
+		for(Map.Entry<Pastel, Integer> entrada : pasteis.entrySet()) {
+			valorBebidas += entrada.getValue() * entrada.getKey().getValor();
+		}
+		valorTotal=  valorBebidas + valorPasteis;
+		this.setValorTotal( valorTotal);
+	}
 }
