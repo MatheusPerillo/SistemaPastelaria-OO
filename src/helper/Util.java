ea8ee460f2342ee299729d315e54e8bd4db0c195
Joao Victor C <correia.jv16@gmail.com>
2021-09-09 22:50:06 -0300

    Adição de máscara para tipo Date

--- src/helper/Util.java
+++ src/helper/Util.java
@@ -5,12 +5,13 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class Util {
 	
-	static SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+	static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	static NumberFormat nf = new DecimalFormat("R$ #,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
 
 	
@@ -24,6 +25,9 @@ public class Util {
 	
 	public static String doubleParaString(Double valor) {
 		return Util.nf.format(valor);
+	}
+	public static String dateParaString(Date date) {
+		return Util.sdf.format(date);
 	}
 	
 	
