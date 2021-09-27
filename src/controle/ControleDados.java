72f7a1acd7d8a7130ab66a226fab3b8c25f5a5f6
MatheusPerillo <c4tr4u1@gmail.com>
2021-09-26 22:46:11 -0300

    Criação do controle dos dados de Pastel e Bebida(inserir, editar..)

--- src/controle/ControleDados.java
+++ src/controle/ControleDados.java
@@ -18,23 +18,27 @@ public class ControleDados {
 	public void setDados(Dados d) {
 		this.d = d;
 	}
-	
+
 	public ArrayList<Cliente> getClientes() {
 		return this.d.getClientes();
 	}
+
 	public ArrayList<Funcionario> getFuncionarios() {
 		return this.d.getFuncionarios();
 	}
+
 	public ArrayList<Pastel> getPasteis() {
 		return this.d.getPasteis();
 	}
+
 	public ArrayList<Bebida> getBebidas() {
 		return this.d.getBebidas();
 	}
+
 	public ArrayList<Venda> getVendas() {
 		return this.d.getVendas();
 	}
-	
+
 	public boolean inserirCliente(Cliente c) {
 		try {
 			this.getClientes().add(c);
@@ -43,11 +47,13 @@ public class ControleDados {
 			return false;
 		}
 	}
-	public boolean editarCliente(int i ,Cliente c) {
+
+	public boolean editarCliente(int i, Cliente c) {
 		try {
 			this.getClientes().get(i).setNome(c.getNome());
 			this.getClientes().get(i).setEmail(c.getEmail());
-			this.getClientes().get(i).setNumCartao(c.getNumCartao());;
+			this.getClientes().get(i).setNumCartao(c.getNumCartao());
+			;
 			this.getClientes().get(i).setEndereco(c.getEndereco());
 			this.getClientes().get(i).setTelefone(c.getTelefone());
 			return true;
@@ -55,6 +61,7 @@ public class ControleDados {
 			return false;
 		}
 	}
+
 	public boolean removerCliente(int i) {
 		try {
 			this.getClientes().remove(i);
@@ -63,7 +70,7 @@ public class ControleDados {
 			return false;
 		}
 	}
-	
+
 	public boolean inserirFuncionario(Funcionario f) {
 		try {
 			this.getFuncionarios().add(f);
@@ -72,8 +79,8 @@ public class ControleDados {
 			return false;
 		}
 	}
-	
-	public boolean editarFuncionario(int i ,Funcionario f) {
+
+	public boolean editarFuncionario(int i, Funcionario f) {
 		try {
 			this.getFuncionarios().get(i).setNome(f.getNome());
 			this.getFuncionarios().get(i).setEmail(f.getEmail());
@@ -88,7 +95,7 @@ public class ControleDados {
 			return false;
 		}
 	}
-	
+
 	public boolean removerFuncionario(int i) {
 		try {
 			this.getFuncionarios().remove(i);
@@ -97,5 +104,73 @@ public class ControleDados {
 			return false;
 		}
 	}
-	
+
+	// ----PASTEL----
+	public boolean inserirPastel(Pastel p) {
+		try {
+			this.getPasteis().add(p);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean editarPastel(int i, Pastel p) {
+		try {
+			this.getPasteis().get(i).setNome(p.getNome());
+			this.getPasteis().get(i).setSabor(p.getSabor());
+			this.getPasteis().get(i).setTamanho(p.getTamanho());
+			this.getPasteis().get(i).setDescricao(p.getDescricao());
+			this.getPasteis().get(i).setValor(p.getValor());
+			this.getPasteis().get(i).setQtdEstoque(p.getQtdEstoque());
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean removerPastel(int i) {
+		try {
+			this.getPasteis().remove(i);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	// ----BEBIDAS----
+	public boolean inserirBebida(Bebida b) {
+		try {
+			this.getBebidas().add(b);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean editarBebida(int i, Bebida b) {
+		try {
+			this.getBebidas().get(i).setNome(b.getNome());
+			this.getBebidas().get(i).setTipo(b.getTipo());
+			this.getBebidas().get(i).setVolume(b.getVolume());
+			this.getBebidas().get(i).setDescricao(b.getDescricao());
+			this.getBebidas().get(i).setValor(b.getValor());
+			this.getBebidas().get(i).setQtdEstoque(b.getQtdEstoque());
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean removerBebida(int i) {
+		try {
+			this.getBebidas().remove(i);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 }
