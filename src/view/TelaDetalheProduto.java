e6176e792ed5182bd9d2a48924de2d70a027212c
MatheusPerillo <c4tr4u1@gmail.com>
2021-09-29 02:23:32 -0300

    Criação de Select para tamanhos e tipos

--- src/view/TelaDetalheProduto.java
+++ src/view/TelaDetalheProduto.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -20,11 +21,11 @@ public class TelaDetalheProduto implements ActionListener {
 	private JLabel labelSabor = new JLabel("Sabor: ");
 	private JTextField valorSabor;
 	private JLabel labelTipo = new JLabel("Tipo: ");
-	private JTextField valorTipo;
+	private JComboBox <String> valorTipo;
 	private JLabel labelVolume = new JLabel("Volume: ");
 	private JTextField valorVolume;
 	private JLabel labelTamanho = new JLabel("Tamanho: ");
-	private JTextField valorTamanho;
+	private JComboBox <String> valorTamanho;
 	private JLabel labelDescricao = new JLabel("Descrição: ");
 	private JTextField valorDescricao;
 	private JLabel labelPreco = new JLabel("Valor: ");
@@ -43,6 +44,9 @@ public class TelaDetalheProduto implements ActionListener {
 		posicao = pos;
 		dados = d;
 
+		String[] tamanhos = {"P","M","G", "GG"};
+		String[] tipos = {"garrafa","copo","garapa", "vidro"};
+		
 		if (op == 1)
 			s = "Cadastro de Pastel";
 		if (op == 2)
@@ -58,66 +62,69 @@ public class TelaDetalheProduto implements ActionListener {
 		if (op == 3) {
 			valorNome = new JTextField(dados.getPasteis().get(pos).getNome(), 200);
 			valorSabor = new JTextField(dados.getPasteis().get(pos).getSabor(), 100);
-			valorTamanho = new JTextField(dados.getPasteis().get(pos).getTamanho(), 2);
+			valorTamanho = new JComboBox <String> (tamanhos);
+			valorTamanho.setSelectedItem(dados.getPasteis().get(pos).getTamanho());
 			valorDescricao = new JTextField(dados.getPasteis().get(pos).getDescricao(), 200);
-			valorPreco = new JTextField(String.valueOf(dados.getPasteis().get(pos).getValor()),50);
-			valorEstoque = new JTextField(String.valueOf(dados.getPasteis().get(pos).getQtdEstoque()),9);
-			valorTipo = new JTextField(100);
+			valorPreco = new JTextField(String.valueOf(dados.getPasteis().get(pos).getValor()), 50);
+			valorEstoque = new JTextField(String.valueOf(dados.getPasteis().get(pos).getQtdEstoque()), 9);
+			valorTipo = new JComboBox <String> (tipos);
+			valorTipo.setSelectedItem(dados.getBebidas().get(pos).getTipo());
 			valorVolume = new JTextField(5);
+			
 			// Preenche dados com dados da bebida que foi clicada
 		} else if (op == 4) {
 			valorNome = new JTextField(dados.getBebidas().get(pos).getNome(), 200);
-			valorTipo = new JTextField(dados.getBebidas().get(pos).getTipo(), 100);
-			valorVolume = new JTextField(String.valueOf(dados.getBebidas().get(pos).getVolume()),5);
+			valorTipo = new JComboBox <String> (tipos);
+			valorVolume = new JTextField(String.valueOf(dados.getBebidas().get(pos).getVolume()), 5);
 			valorDescricao = new JTextField(dados.getBebidas().get(pos).getDescricao(), 200);
-			valorPreco = new JTextField(String.valueOf(dados.getBebidas().get(pos).getValor()),50);
-			valorEstoque = new JTextField(String.valueOf(dados.getBebidas().get(pos).getQtdEstoque()),9);
+			valorPreco = new JTextField(String.valueOf(dados.getBebidas().get(pos).getValor()), 50);
+			valorEstoque = new JTextField(String.valueOf(dados.getBebidas().get(pos).getQtdEstoque()), 9);
 			valorSabor = new JTextField(100);
-			valorTamanho = new JTextField(2);
+			valorTamanho = new JComboBox <String> (tamanhos);
 
 			// Não preenche com dados
 		} else {
 
 			valorNome = new JTextField(200);
 			valorSabor = new JTextField(100);
-			valorTamanho = new JTextField(2);
+			valorTamanho = new JComboBox <String> (tamanhos);
 			valorDescricao = new JTextField(200);
 			valorPreco = new JTextField(50);
 			valorEstoque = new JTextField(9);
-			valorTipo = new JTextField(100);
+			valorTipo = new JComboBox <String> (tipos);
 			valorVolume = new JTextField(5);
 
 			if (op == 1) {
-				botaoSalvar.setBounds(120, 295, 115, 30);
+				botaoSalvar.setBounds(125, 220, 115, 30);
 			} else if (op == 2) {
-				botaoSalvar.setBounds(120, 380, 115, 30);
+				botaoSalvar.setBounds(125, 220, 115, 30);
 			}
 
 		}
 
 		labelNome.setBounds(30, 20, 150, 25);
-		valorNome.setBounds(180, 20, 180, 25);
+		valorNome.setBounds(110, 20, 180, 25);
 
-		labelSabor.setBounds(30, 50, 150, 25);
-		valorSabor.setBounds(180, 50, 180, 25);
+		labelDescricao.setBounds(30, 50, 150, 25);
+		valorDescricao.setBounds(110, 50, 180, 25);
 
-		labelTamanho.setBounds(30, 80, 150, 25);
-		valorTamanho.setBounds(180, 80, 180, 25);
+		labelSabor.setBounds(30, 80, 150, 25);
+		valorSabor.setBounds(110, 80, 180, 25);
 
-		labelDescricao.setBounds(30, 110, 150, 25);
-		valorDescricao.setBounds(180, 110, 180, 25);
+		labelTamanho.setBounds(30, 110, 150, 25);
+		valorTamanho.setBounds(110, 110, 180, 25);
+
+		labelTipo.setBounds(30, 80, 150, 25);
+		valorTipo.setBounds(110, 80, 180, 25);
+
+		labelVolume.setBounds(30, 110, 150, 25);
+		valorVolume.setBounds(110, 110, 180, 25);
 
 		labelPreco.setBounds(30, 140, 150, 25);
-		valorPreco.setBounds(180, 140, 180, 25);
+		valorPreco.setBounds(110, 140, 180, 25);
 
 		labelEstoque.setBounds(30, 170, 150, 25);
-		valorEstoque.setBounds(180, 170, 180, 25);
-
-		labelTipo.setBounds(30, 200, 150, 25);
-		valorTipo.setBounds(180, 200, 180, 25);
-
-		labelVolume.setBounds(30, 230, 150, 25);
-		valorVolume.setBounds(180, 230, 180, 25);
+		valorEstoque.setBounds(110, 170, 27, 25);
 
 		// Coloca os campos relacionados a pastel
 		if (op == 1 || op == 3) {
@@ -134,19 +141,19 @@ public class TelaDetalheProduto implements ActionListener {
 			this.janela.add(valorVolume);
 			this.janela.add(labelTipo);
 			this.janela.add(valorTipo);
-
+		}
 			// Coloca botoes de excluir e salvar
 			if (op == 3) {
-				botaoSalvar.setBounds(120, 295, 115, 30);
-				botaoExcluir.setBounds(245, 295, 115, 30);
+				botaoSalvar.setBounds(60, 217, 115, 30);
+				botaoExcluir.setBounds(190, 217, 115, 30);
 				this.janela.add(botaoExcluir);
 			} else if (op == 4) {
-				botaoSalvar.setBounds(120, 380, 115, 30);
-				botaoExcluir.setBounds(245, 380, 115, 30);
+				botaoSalvar.setBounds(60, 217, 115, 30);
+				botaoExcluir.setBounds(190, 217, 115, 30);
 				this.janela.add(botaoExcluir);
 			}
-		}
 		
+
 		this.janela.add(labelNome);
 		this.janela.add(valorNome);
 		this.janela.add(labelPreco);
@@ -155,19 +162,23 @@ public class TelaDetalheProduto implements ActionListener {
 		this.janela.add(valorDescricao);
 		this.janela.add(labelEstoque);
 		this.janela.add(valorEstoque);
-		
+		this.janela.add(botaoSalvar);
+
 		this.janela.setLayout(null);
-		
-		if(op== 2 || op==4) {this.janela.setSize(500, 470);}
-		else if(op==1 || op==3) {this.janela.setSize(420, 270);}
+
+		if (op == 2 || op == 4) {
+			this.janela.setSize(380, 310);
+		} else if (op == 1 || op == 3) {
+			this.janela.setSize(380, 310);
+		}
 		this.janela.setVisible(true);
-		
+
 		botaoSalvar.addActionListener(this);
 		botaoExcluir.addActionListener(this);
-		
+
 	}
 
-	public void actionPerformed(ActionEvent e) {	
+	public void actionPerformed(ActionEvent e) {
 		Object src = e.getSource();
 		if (src == botaoSalvar) {
 			try {
@@ -178,20 +189,47 @@ public class TelaDetalheProduto implements ActionListener {
 				double preco = Double.valueOf(valorPreco.getText());
 				int estoque = Integer.valueOf(valorEstoque.getText());
 				
+				// inserir e editar Pastel
+				if(opcao == 1) {
+					 String sabor = valorSabor.getText();
+					 String tamanho = (String) valorTamanho.getSelectedItem();
+					 Pastel p = new Pastel(nome, preco, descricao, estoque, tamanho, sabor);
+					 res = dados.inserirPastel(p);
+					 
+				}else if(opcao == 3) {
+					 String sabor = valorSabor.getText();
+					 String tamanho = (String) valorTamanho.getSelectedItem();
+					 Pastel p = new Pastel(nome, preco, descricao, estoque, tamanho, sabor);
+					 res = dados.editarPastel(posicao, p);
+				}
+					
+				// inserir e editar Bebida
 				
-				//String tipo = valorTipo.getText();
-				//int volume = Integer.valueOf(valorVolume.getText());
-				//String sabor = valorSabor.getText();
-				//String tamanho = valorTamanho.getText();
-				
-				
-			}catch (NullPointerException exc1) {
+				if (opcao == 2) {
+					 String tipo = (String) valorTipo.getSelectedItem();
+					 int volume = Integer.valueOf(valorVolume.getText());
+					 Bebida b = new Bebida(nome, preco, descricao, estoque, volume, tipo);
+					 res = dados.inserirBebida(b);
+					 
+				}else if(opcao == 4) {
+					 String tipo = (String) valorTipo.getSelectedItem();
+					 int volume = Integer.valueOf(valorVolume.getText());
+					 Bebida b = new Bebida(nome, preco, descricao, estoque, volume, tipo);
+					 res = dados.editarBebida(posicao, b);
+				}
+					
+					if(res) {
+						mensagemSucessoCadastro();
+					} else
+						mensagemErroCadastro();
+
+			} catch (NullPointerException exc1) {
 				mensagemErroCadastro();
-			}catch(NumberFormatException exc2) {
+			} catch (NumberFormatException exc2) {
 				mensagemErroCadastro();
 			}
 		}
-		
+
 		if (src == botaoExcluir) {
 			boolean res = false;
 
@@ -200,19 +238,21 @@ public class TelaDetalheProduto implements ActionListener {
 				if (res)
 					mensagemSucessoExclusao();
 				else
-					mensagemErroExclusaoPastel();
+					mensagemErroExclusao();
 			}
 
-			if (opcao == 4){ //exclui bebida
+			if (opcao == 4) { // exclui bebida
 				res = dados.removerBebida(posicao);
-				if (res) mensagemSucessoExclusao(); 
-				else mensagemErroExclusaoBebida(); 
+				if (res)
+					mensagemSucessoExclusao();
+				else
+					mensagemErroExclusao();
 			}
 
 		}
-		
+
 	}
-	
+
 	public void mensagemSucessoExclusao() {
 		JOptionPane.showMessageDialog(null, "Os dados foram excluidos com sucesso!", null,
 				JOptionPane.INFORMATION_MESSAGE);
@@ -229,21 +269,15 @@ public class TelaDetalheProduto implements ActionListener {
 		JOptionPane.showMessageDialog(null,
 				"ERRO AO SALVAR OS DADOS!\n " + "Pode ter ocorrido um dos dois erros a seguir:  \n"
 						+ "1. Nem todos os campos foram preenchidos \n"
-						+ "2. Volume, preço e Estoque não contém apenas números",
+						+ "2. Volume, Valor ou  Estoque não contém apenas números \n" 
+						+ "3. Foi digitado uma vírgula (,) no lugar de um ponto (.) em Valor ",
 				null, JOptionPane.ERROR_MESSAGE);
 	}
 
-	public void mensagemErroExclusaoPastel() {
+	public void mensagemErroExclusao() {
 		JOptionPane.showMessageDialog(null,
-				"Ocorreu um erro ao excluir o dado.\n " + "Verifique se o aluno está matriculado\n"
-						+ "em alguma disciplina. Se sim, cancele\n " + "a matricula e tente novamente.",
+				"Ocorreu um erro ao excluir o dado.\n ",
 				null, JOptionPane.ERROR_MESSAGE);
 	}
 
-	public void mensagemErroExclusaoBebida() {
-		JOptionPane.showMessageDialog(null,
-				"Ocorreu um erro ao excluir o dado.\n " + "Verifique se o professor está responsável\n"
-						+ "por alguma disciplina. Se sim, substitua\n " + "o professor e tente novamente.",
-				null, JOptionPane.ERROR_MESSAGE);
-	}
 }
