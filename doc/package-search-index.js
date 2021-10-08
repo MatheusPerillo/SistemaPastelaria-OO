2dcdba78a8b15a6d66071ebbc425582b84dc393c
Joao Victor C <correia.jv16@gmail.com>
2021-10-08 02:24:47 -0300

    Geração de documentação

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controle"},{"l":"helper"},{"l":"modelo"},{"l":"pacoteDeTeste"},{"l":"view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pacoteDeTeste"}]
