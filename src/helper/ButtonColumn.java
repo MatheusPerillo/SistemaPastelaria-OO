cedcb956e9dfd02e182f02b823b8ce368864218a
Joao Victor C <correia.jv16@gmail.com>
2021-10-08 01:18:21 -0300

    Implementação de documentação da classe ButtonColumn

--- src/helper/ButtonColumn.java
+++ src/helper/ButtonColumn.java
@@ -17,6 +17,14 @@ import controle.ControleDados;
 import controle.ControleVenda;
 import modelo.Venda;
 
+/**
+ * Classe para possibilitar a criação de uma coluna com botões de excluir em uma
+ * JTable
+ * 
+ * @author Joao Victor Correia
+ * @author Matheus Perillo
+ * @version 1.0(out,2021)
+ */
 public class ButtonColumn extends AbstractCellEditor implements TableCellRenderer, TableCellEditor, ActionListener {
 	JTable table;
 	JButton renderButton;
@@ -24,6 +32,13 @@ public class ButtonColumn extends AbstractCellEditor implements TableCellRendere
 	String text;
 	ControleDados dados;
 
+	/**
+	 * Construtor para instaciação do objeto ButtonColumn
+	 * 
+	 * @param table	JTable que a coluna será adicionada
+	 * @param column	número da coluna da tabela que será ocupada
+	 * @param d	dados presentes no sistema
+	 */
 	public ButtonColumn(JTable table, int column, ControleDados d) {
 		super();
 		this.dados = d;
@@ -39,6 +54,17 @@ public class ButtonColumn extends AbstractCellEditor implements TableCellRendere
 		columnModel.getColumn(column).setCellEditor(this);
 	}
 
+	/**
+	 * Retorna o componente usado para desenhar a célula. Este método é usado para
+	 * configurar o renderizador apropriadamente antes de desenhar
+	 * 
+	 * @param table,      JTable em que ocorrerá alterações
+	 * @param value,      Valor da célula a ser renderizada
+	 * @param isSelected, verdadeiro se a célula deve ser renderizada com a seleção
+	 *                    realçada, caso contrário, falso
+	 * @param hasFocus,   se verdadeiro, renderiza a célula apropriadamente
+	 * @return Component, componente a ser renderizado
+	 */
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
 			int row, int column) {
 		if (hasFocus) {
@@ -56,24 +82,43 @@ public class ButtonColumn extends AbstractCellEditor implements TableCellRendere
 		return renderButton;
 	}
 
+	/**
+	 * Retorna o componente que deve ser adicionado.Este componente será capaz de
+	 * desenhar e receber a entrada do usuário.
+	 * 
+	 * @param table,                JTable em que ocorrerá alterações
+	 * @param value,                Valor da célula a ser editada
+	 * @param isSelected,verdadeiro se a célula deve ser renderizada com destaque
+	 * @param row,                  linha da célula a ser editada
+	 * @param column,               coluna da célula a ser editada
+	 * @return Component, componente a ser editado
+	 */
 	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
 		text = (value == null) ? "Excluir" : value.toString();
 		editButton.setText(text);
 		return editButton;
 	}
 
+	/**
+	 * Método para retornar o valor contido no editor
+	 * 
+	 * @return valor contido no editor
+	 */
 	public Object getCellEditorValue() {
 		return text;
 	}
-
+	/** 
+	 * Método para realizar uma ação quando ocorre um evento: 
+	 *<br>
+	 * Ação- Remove do objeto Dados a venda presente na linha da tabela     
+	 */
 	public void actionPerformed(ActionEvent e) {
 		fireEditingStopped();
-		 DefaultTableModel model = (DefaultTableModel) this.table.getModel();
-		 int id= (int) table.getValueAt(table.getSelectedRow(), 0);
-		 model.removeRow(table.getSelectedRow());
-		Venda v= new ControleVenda(dados).buscarPorId(id);
+		DefaultTableModel model = (DefaultTableModel) this.table.getModel();
+		int id = (int) table.getValueAt(table.getSelectedRow(), 0);
+		model.removeRow(table.getSelectedRow());
+		Venda v = new ControleVenda(dados).buscarPorId(id);
 		dados.removerVenda(v);
-		 
-		 
-	} 
+
+	}
 }
